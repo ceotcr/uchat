42396bbe38031399d7ce44c1149e3264e69d5597
Chetan (TCR) Sapkal <68210482+ceotcr@users.noreply.github.com>
2024-07-20 18:11:53 +0530

    Add project files.

---
+++ Controllers/ChatController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace uchat.Controllers
+{
+    public class ChatController: Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GroupChat(
+            string roomid    
+        )
+        {
+            ViewData["roomid"] = roomid;
+            return View();
+        }
+    }
+
+}
---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace uchat.Hubs
+{
+	public class ChatHub : Hub
+	{
+		public async Task SendMessage(string user, string message)
+	   => await Clients.All.SendAsync("ReceiveMessage", user, message);
+
+		public async Task SendPrivateMessage(string fromUser, string toUser, string message)
+			=> await Clients.User(toUser).SendAsync("ReceivePrivateMessage", fromUser, message);
+		public async Task JoinGroup(string roomid, string username)
+		{
+			await Groups.AddToGroupAsync(Context.ConnectionId, roomid);
+			await Clients.Group(roomid).SendAsync("ReceiveGroupMessage", "System", $"{username} has joined the room");
+		}
+
+		public async Task SendMessageToGroup(string roomid, string username, string message)
+        {
+            await Clients.Group(roomid).SendAsync("ReceiveGroupMessage", username, message);
+        }
+		public async Task LeaveGroup(string roomid, string username)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomid);
+            await Clients.Group(roomid).SendAsync("ReceiveGroupMessage", "System", $"{username} has left the room");
+        }
+	}
+}
